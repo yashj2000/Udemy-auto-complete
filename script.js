4a093990c73bbd2edca1fb825006100cd995353b
Yash Janyani <54151714+yashj2000@users.noreply.github.com>
2022-08-14 00:36:54 +0530

    added comments

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Stores all unexpanded section headers, and clicks on the unexpanded section headers to expand them
 const sectionEl = document.querySelectorAll("section[data-purpose='sidebar'] div.udlite-btn");
 sectionEl.forEach((section) => {
   const isClosed = section.parentElement.querySelector("span").getAttribute("data-checked") !== "checked"
   if (isClosed) section.click()
 });
 
-// Stores all checkboxes on the page, and clicks on any checkboxes on that currently checked
+// Stores all checkboxes on the page in an array, and clicks on the checkboxes that are unchecked
 const checkboxes = document.querySelectorAll("input[type='checkbox']");
 checkboxes.forEach((checkbox) => {
   if(!checkbox.checked) {
